103edea0373ad45bfc84e0990d82533848bef55f
João Manoel <jmtac783@gmail.com>
2026-03-11 10:10:03 -0300

    update

---
+++ click-events/src/App.jsx
@@ -0,0 +1,15 @@
+// Para gerenciar eventos de clique no React, utiliza-se o manipulador de eventos onClick. Diferente do HTML nativo que usa onclick 
+// em minúsculas e espera uma string, o React usa a sintaxe camelCase (onClick) e espera uma função (callback) como valor entre 
+// chaves. 
+
+import Botao from "./Botao"
+
+function App() {
+  return (
+    <>
+      <Botao />
+    </>
+  );
+}
+
+export default App
---
+++ click-events/src/Botao.jsx
@@ -0,0 +1,7 @@
+function Botao() {
+    return (
+        <button onClick={}>me clique</button>
+    );
+}
+
+export default Botao;
