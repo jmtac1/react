d28f8ba14fbeddda4269339db5a7c2b4da22cb92
João Manoel <jmtac783@gmail.com>
2026-03-11 13:45:07 -0300

    update

---
+++ hooks/src/App.jsx
@@ -0,0 +1,15 @@
+// Os Hooks são funções especiais introduzidas no React 16.8 que permitem usar o state (estado) e outros recursos do React em
+// componentes funcionais, sem a necessidade de escrever classes.
+// (useState, useEffect, useContext, useReducer, and more...) 
+
+// O useState() é um Hook do React que permite adicionar estado local a componentes funcionais. Ele é fundamental para gerenciar 
+// dados dinâmicos em sua aplicação, como contadores, entradas de formulários ou qualquer valor que precise ser preservado entre 
+// re-renderizações. 
+
+import Componente from "./Componente";
+
+function App() {
+
+}
+
+export default App;
---
+++ hooks/src/Componente.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function Componente() {
+    const [] = useState();
+}
+
+export default Componente;
