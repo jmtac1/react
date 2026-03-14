16d3ab582269a189724f371e1dea51b46b28831f
João Manoel <jmtac783@gmail.com>
2026-03-14 19:53:36 -0300

    update

---
+++ updater-function/src/App.jsx
@@ -0,0 +1,9 @@
+// A função de atualização (updater function) é uma função especial que pode ser passada para a função setter de estado no React 
+// (por exemplo, setCount no useState). Ela é usada para garantir que a atualização do estado seja baseada no estado pendente/
+// anterior mais recente, o que é crucial devido à natureza assíncrona das atualizações de estado no React. 
+
+function App() {
+  
+}
+
+export default App;
