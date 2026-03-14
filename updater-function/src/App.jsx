26ddff35eebcf4ccb9337fa283e874a54776087d
João Manoel <jmtac783@gmail.com>
2026-03-14 20:15:05 -0300

    Update App.jsx

--- updater-function/src/App.jsx
+++ updater-function/src/App.jsx
@@ -2,8 +2,35 @@
 // (por exemplo, setCount no useState). Ela é usada para garantir que a atualização do estado seja baseada no estado pendente/
 // anterior mais recente, o que é crucial devido à natureza assíncrona das atualizações de estado no React. 
 
+import { useState } from "react";
+
 function App() {
-  
+  const [cont, setCont] = useState(0);
+
+  const incrementar = () => {
+    setCont(prevCont => prevCont + 1);
+    setCont(prevCont => prevCont + 1);
+    setCont(prevCont => prevCont + 1);
+  };
+
+  const reset = () => {
+    setCont(0);
+  };
+
+  const decremento = () => {
+    setCont(prevCont => prevCont - 1);
+    setCont(prevCont => prevCont - 1);
+    setCont(prevCont => prevCont - 1);
+  };
+
+  return (
+    <div>
+      <h1>{cont}</h1>
+      <button onClick={incrementar}>incrementar</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={decremento}>decremento</button>
+    </div>
+  );
 }
 
 export default App;
