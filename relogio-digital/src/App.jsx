fa057470d06d6bbcd2e222904b44347c68028616
João Manoel <jmtac783@gmail.com>
2026-03-31 18:06:37 -0300

    update

--- relogio-digital/src/App.jsx
+++ relogio-digital/src/App.jsx
@@ -1,9 +1,10 @@
+import Relogio from "./Relogio"
 
 function App() {
 
   return (
     <>
-      
+      <Relogio />
     </>
   )
 }
--- relogio-digital/src/Relogio.jsx
+++ relogio-digital/src/Relogio.jsx
@@ -1,10 +1,37 @@
+import { useState, useEffect } from "react";
+
 function Relogio() {
 
+    const [tempo, setTempo] = useState(new Date());
+
+    useEffect(() => {
+        const intervalo = setInterval(() => {
+            setTempo(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalo);
+        }
+    }, []);
+
+    function tempoFormatado() {
+        let horas = tempo.getHours();
+        const minutos = tempo.getMinutes();
+        const segundos = tempo.getSeconds();
+
+        return `${padZero(horas)}:${padZero(minutos)}:${padZero(segundos)}`
+    }
+
+    function padZero(numero) {
+        return (numero < 10 ? '0' : '') + numero;
+    }
 
     return (
-        <>
-
-        </>
+        <div className="container-relogio">
+            <div className="relogio">
+                <span>{tempoFormatado()}</span>
+            </div>
+        </div>
     );
 }
 
